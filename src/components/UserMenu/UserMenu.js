1fcad6ce70b73b0d5403cf00887db4650d7b533e
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-27 18:26:17 +0300

    l0-0-

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,15 +1,14 @@
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import { selectMenu } from 'redux/selectors';
-import { NavLink } from 'react-router-dom';
+
 import { useAuth } from 'Hooks/auth-use';
 import UserMenu from 'components/UserMenu/UserMenu';
 import AuthMenu from 'components/AuthMenu/AuthMenu';
 
-import { closeMenu } from 'redux/reducers/menuSlice';
+
 
 const BurgerMenu = () => {
   const isOpenMenu = useSelector(selectMenu);
-  const dispatch = useDispatch();
   const { isLoggedIn } = useAuth();
   // const menuRef = useRef(null);
 
