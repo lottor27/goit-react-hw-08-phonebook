ab499b81a51a64a1fed5611a623d0e31a97fb2c9
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-27 16:47:07 +0000

    Deploying to gh-pages from @ lottor27/goit-react-hw-08-phonebook@a07a4d7696b73f4b8d707cd3d324c31f4feac4f1 🚀

--- static/js/732.a60e3ebb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[732],{4732:function(e,t,o){o.r(t);var n=o(2791),u=o(7689),c=o(3329);t.default=function(){var e=(0,u.s0)();return(0,n.useEffect)((function(){var t=setTimeout((function(){e("/")}),4e3);return function(){clearTimeout(t)}}),[e]),(0,c.jsx)("h2",{className:"text-message",children:"404 Not Found. Will be redicted to the home page!"})}}}]);
-//# sourceMappingURL=732.a60e3ebb.chunk.js.map
