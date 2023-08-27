699225ea2d71adab1d05c8687fa27a13b2dc2d06
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-08-27 08:28:10 +0300

    asd

--- src/components/Heder/Heder.jsx
+++ src/components/Heder/Heder.jsx
@@ -19,7 +19,7 @@ const Header = ({ headTitle, headSubTitle }) => {
         <div>
           <div>
             <Link
-              to="/"
+              to="/goit-react-hw-08-phonebook"
               className="navbar-toggler"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNav"
@@ -32,11 +32,10 @@ const Header = ({ headTitle, headSubTitle }) => {
             <p className="navbar-brand">{headSubTitle}</p>
           </div>
         </div>
-        
+
         <div className="nav-link active">
           {isLoggedIn ? <UserMenu /> : <AuthMenu />}
         </div>
-        
       </div>
     </div>
   );
